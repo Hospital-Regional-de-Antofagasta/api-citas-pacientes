2db22b6f226652f578aa4a5ce9b67948e1902178
Pablo Fontecilla <pafonbu@gmail.com>
2021-05-19 20:52:47 +0000

    HRAP-112 CHANGE: control de versiones

--- api/index.js
+++ api/index.js
@@ -1,18 +1,21 @@
-const setTZ = require('set-tz')
-setTZ('UTC')
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const { loadConfig } = require('./config')
-const citasPacientes = require('./routes/citasPacientes')
-const app =express()
-app.use(express.json())
-app.use(cors())
+const setTZ = require("set-tz");
+setTZ("UTC");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { loadConfig } = require("./config");
+const citasPacientes = require("./routes/citasPacientes");
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-loadConfig()
+loadConfig();
 
-app.use('/citas_pacientes',citasPacientes)
+app.use("/v1/citas_pacientes", citasPacientes);
 
-module.exports = app
+module.exports = app;
--- api/models/CitasPacientes.js
+++ api/models/CitasPacientes.js
@@ -1,7 +1,9 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const CitasPacientes = mongoose.model('citas_paciente', new Schema({
+const CitasPacientes = mongoose.model(
+  "citas_paciente",
+  new Schema({
     correlativoCita: Number,
     nombreLugar: String,
     codigoServicio: String,
@@ -14,8 +16,9 @@ const CitasPacientes = mongoose.model('citas_paciente', new Schema({
     codigoAmbito: String,
     tipoCita: String,
     alta: {
-        type: Boolean,
-        default: false
-    }
-}))
-module.exports = CitasPacientes
+      type: Boolean,
+      default: false,
+    },
+  })
+);
+module.exports = CitasPacientes;
