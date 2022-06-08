b22a5b0c0d30c86d388546b939efd182b10e554c
pafonbu <pafonbu@gmail.com>
2022-06-08 14:18:09 -0400

    audit fix

--- api/models/CitasPacientes.js
+++ api/models/CitasPacientes.js
@@ -3,23 +3,26 @@ const Schema = mongoose.Schema;
 
 const CitasPacientes = mongoose.model(
   "citas_paciente",
-  new Schema({
-    correlativo: { type: Number, required: true },
-    codigoLugar: { type: String, required: true },
-    nombreLugar: String,
-    codigoServicio: { type: String, required: true },
-    nombreServicio: String,
-    codigoProfesional: { type: String, required: true },
-    nombreProfesional: String,
-    tipo: { type: String, required: true },
-    codigoAmbito: { type: String, required: true },
-    fechaCitacion: { type: Date, required: true },
-    horaCitacion: { type: String, required: true },
-    rutPaciente: { type: String, required: true, select: false },
-    alta: { type: Boolean, default: false },
-    bloqueadaEl: Date,
-    codigoEstablecimiento: { type: String, required: true },
-    nombreEstablecimiento: { type: String, required: true },
-  })
+  new Schema(
+    {
+      correlativo: { type: Number, required: true },
+      codigoLugar: { type: String, required: true },
+      nombreLugar: String,
+      codigoServicio: { type: String, required: true },
+      nombreServicio: String,
+      codigoProfesional: { type: String, required: true },
+      nombreProfesional: String,
+      tipo: { type: String, required: true },
+      codigoAmbito: { type: String, required: true },
+      fechaCitacion: { type: Date, required: true },
+      horaCitacion: { type: String, required: true },
+      rutPaciente: { type: String, required: true, select: false },
+      alta: { type: Boolean, default: false },
+      bloqueadaEl: Date,
+      codigoEstablecimiento: { type: String, required: true },
+      nombreEstablecimiento: { type: String, required: true },
+    },
+    { timestamps: true }
+  )
 );
 module.exports = CitasPacientes;
